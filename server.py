771bfcf6d3ba2757b3f6972cb6202f920261cb4b
NITHEESHWAR B R <nitheeshwar.r.g@gmail.com>
2023-09-15 12:33:31 +0530

    server integration server 2

--- server.py
+++ server.py
@@ -1,8 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Request,Depends
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 import importlib
 from sentiment_analyzer import sentiment_analysis
+from fastapi.templating import Jinja2Templates
+from starlette.middleware.trustedhost import TrustedHostMiddleware
+from typing import List, Dict, Union
+app = FastAPI()
+app.add_middleware(TrustedHostMiddleware, allowed_hosts=["*"])
+templates = Jinja2Templates(directory="templates")
+
 
 mod=importlib.import_module("facebook")
 analytics=[]
@@ -11,17 +18,31 @@ app=FastAPI()
 class Data(BaseModel):
     link: str | None=None
     data: list | None=None
+class CommentSentiment(BaseModel):
+    comments: str
+    negative: float
+    neutral: float
+    positive: float
+@app.get("/")
+async def root(request:Request):
+    return templates.TemplateResponse('temporary.html',{"request":request})
 
 
-@app.post("/")
-async def root(data: Data=None):
-    if data is None:
+@app.post("/", response_model=list)
+async def processed_data(data: Data):
+    if data is None or (data.link is None and data.data is None):
         return JSONResponse(content={"error_message": "Pass the required values"}, status_code=400)
-    if(data.link==None):
-        response=data.data
-    else:   
-        response=await mod.facebookComments(data.link,"uat")
+    
+    if data.link is not None:
+        # Use data.link for processing
+        response = await mod.facebookComments(data.link, "uat")
+    else:
+        # Use data.data for processing
+        response = data.data
+
+    result = []
     for i in response:
-        sentiments=sentiment_analysis(i)
-        analytics.append({"comments":i,"negative":sentiments[0],"neutral":sentiments[1],"positive":sentiments[2]})
-    return JSONResponse(content=analytics, status_code=200)
+        sentiments = sentiment_analysis(i)
+        result.append({"comments": i, "negative": sentiments[0], "neutral": sentiments[1], "positive": sentiments[2]})
+        print(result)
+    return {"result": result}
