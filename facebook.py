f2abd92093612dc079eb63a00aae2343d6f8f732
Abhishek32971 <abhishekrp32971@gmail.com>
2023-09-15 10:18:48 +0530

    updating code after pulling

--- facebook.py
+++ facebook.py
@@ -1,25 +1,25 @@
 
 from selenium import webdriver
-import time 
+# import time 
 from selenium.webdriver.common.by import By
-from datetime import datetime
+# from datetime import datetime
 from selenium.webdriver.chrome.options import Options
-import requests
-from datetime import datetime
-import logging
-import os
-import uuid
-from dotenv import load_dotenv
+# import requests
+# from datetime import datetime
+# import logging
+# import os
+# import uuid
+# from dotenv import load_dotenv
 import regex as re
-from datetime import datetime
-import csv
+# from datetime import datetime
+# import csv
 
 
 async def facebookComments(link,envir):
     if envir=="uat":
-        CALLBACK_URL = os.environ.get("CALLBACKURL")
+        # CALLBACK_URL = os.environ.get("CALLBACKURL")
 
-        logging.basicConfig(level=logging.INFO,filename="log.log",filemode="w",format="%(asctime)s  -  %(levelname)s   -   %(message)s")
+        # logging.basicConfig(level=logging.INFO,filename="log.log",filemode="w",format="%(asctime)s  -  %(levelname)s   -   %(message)s")
 
        
         # website="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fpermalink.php%3Fstory_fbid%3Dpfbid034U4BEygEMb52mio1z91BJ97Gd94Ju8LY3j1nKSRWGMjHaScovwZ6jqhq4afM9TELl%26id%3D61550896213623&show_text=true&width=500"
@@ -31,16 +31,16 @@ async def facebookComments(link,envir):
         driver = webdriver.Chrome(options=chrome_options)     
         driver.get(link)
         #sleep after loading website 
-        time.sleep(5)
-        logging.info("website loaded")
+        # time.sleep(5)
+        # logging.info("website loaded")
 
         #click on comments
         element = driver.find_element(By.CLASS_NAME,'_29bd')
         href_value = element.get_attribute('href')
         driver.get(href_value)
-        time.sleep(5)
+        # time.sleep(5)
         
-        userList=[]
+        # userList=[]
         Comment_list=[]
         comments= driver.find_elements(By.CLASS_NAME,'x1y1aw1k')
 
@@ -58,7 +58,7 @@ async def facebookComments(link,envir):
                 if(len(lines)<2):
                     continue
                 Comment_list.append("".join(lines[1:]))
-                userList.append(lines[0])
+                # userList.append(lines[0])
 
             else:
                 lines=text.strip().splitlines()
