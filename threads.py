81c4a3dec02665633f1a42ae27c4f2a1cdbe90cf
Abhishek32971 <abhishekrp32971@gmail.com>
2023-09-15 11:22:26 +0530

    threads fix

--- threads.py
+++ threads.py
@@ -1,21 +1,21 @@
 from selenium import webdriver
-import time 
+# import time 
 from selenium.webdriver.common.by import By
-from datetime import datetime
+# from datetime import datetime
 from selenium.webdriver.chrome.options import Options
-import requests
-from datetime import datetime
+# import requests
+# from datetime import datetime
 import logging
-import os
-import uuid
-from dotenv import load_dotenv
-import regex as re
-from datetime import datetime
-import csv
+# import os
+# import uuid
+# from dotenv import load_dotenv
+# import regex as re
+# from datetime import datetime
+# import csv
 
-def thredComments(link,envir):
+def Comments(link,envir):
     if envir=="uat":
-        CALLBACK_URL = os.environ.get("CALLBACKURL")
+        # CALLBACK_URL = os.environ.get("CALLBACKURL")
         # print(CALLBACK_URL)
         # time.sleep(5)
         logging.basicConfig(level=logging.INFO,filename="log.log",filemode="w",format="%(asctime)s  -  %(levelname)s   -   %(message)s")
@@ -30,16 +30,16 @@ def thredComments(link,envir):
         driver = webdriver.Chrome(options=chrome_options)     
         driver.get(link)
         #sleep after loading website 
-        time.sleep(5)
+        # time.sleep(5)
         logging.info("website loaded")
-        userNames=[]
-        userElement = driver.find_elements(By.CSS_SELECTOR,'.x1i10hfl.xjbqb8w.x6umtig.x1b1mbwd.xaqea5y.xav7gou.x9f619.x1ypdohk.xt0psk2.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x16tdsg8.x1hl2dhg.xggy1nq.x1a2a7pz.xp07o12.xzmqwrg.x1citr7e.x1kdxza.xt0b8zv')
+        # userNames=[]
+        # userElement = driver.find_elements(By.CSS_SELECTOR,'.x1i10hfl.xjbqb8w.x6umtig.x1b1mbwd.xaqea5y.xav7gou.x9f619.x1ypdohk.xt0psk2.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x16tdsg8.x1hl2dhg.xggy1nq.x1a2a7pz.xp07o12.xzmqwrg.x1citr7e.x1kdxza.xt0b8zv')
         # print(element)
-        for i in userElement:
-            title =i.text
-            userNames.append(title)
-            # print("title:",title)
-        # print(userNames[:5])
+        # for i in userElement:
+        #     title =i.text
+        #     userNames.append(title)
+        #     # print("title:",title)
+        # # print(userNames[:5])
         commentElement=[]
         element = driver.find_elements(By.CSS_SELECTOR,'span.x1lliihq.x1plvlek.xryxfnj.x1n2onr6.x193iq5w.xeuugli.x1fj9vlw.x13faqbe.x1vvkbs.x1s928wv.xhkezso.x1gmr53x.x1cpjm7i.x1fgarty.x1943h6x.x1i0vuye.xjohtrz.xo1l8bm.xp07o12.xat24cr.xdj266r')
         # print(element)
@@ -48,16 +48,16 @@ def thredComments(link,envir):
             commentElement.append(title)
             # print("title:",title)
         # print(commentElement[:5])
-        csv_file_path="my_data.csv"
-        with open(csv_file_path, mode='w', newline='',encoding="utf-8") as csv_file:
-        # Create a CSV writer
-            csv_writer = csv.writer(csv_file)
+        # csv_file_path="my_data.csv"
+        # with open(csv_file_path, mode='w', newline='',encoding="utf-8") as csv_file:
+        # # Create a CSV writer
+        #     csv_writer = csv.writer(csv_file)
 
-            # Write the header row
-            csv_writer.writerow(['UserName','Summary'])  # Replace with your column headers
+        #     # Write the header row
+        #     csv_writer.writerow(['UserName','Summary'])  # Replace with your column headers
 
-            # Use zip to iterate through the lists simultaneously and write them as rows
-            for data_row in zip(userNames,commentElement):
-                csv_writer.writerow(data_row)
-
-thredComments("https://www.threads.net/@ishansharma7390/post/CwDhaXevirT","uat")
+        #     # Use zip to iterate through the lists simultaneously and write them as rows
+        #     for data_row in zip(userNames,commentElement):
+        #         csv_writer.writerow(data_row)
+        return commentElement
+Comments("https://www.threads.net/@ishansharma7390/post/CwDhaXevirT","uat")
